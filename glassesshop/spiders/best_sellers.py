cb9b3e7f0e582ee1ece3a91e3ec5584c4d435056
Davide Cerato <mrc3drik@gmail.com>
2020-06-01 09:34:10 +0200

    scraped products

--- glassesshop/spiders/best_sellers.py
+++ glassesshop/spiders/best_sellers.py
@@ -4,8 +4,14 @@ import scrapy
 
 class BestSellersSpider(scrapy.Spider):
     name = 'best_sellers'
-    allowed_domains = ['www.glassesshop.com/bestsellers']
-    start_urls = ['http://www.glassesshop.com/bestsellers/']
+    allowed_domains = ['www.glassesshop.com']
+    start_urls = ['https://www.glassesshop.com/bestsellers/']
 
     def parse(self, response):
-        pass
+        glasses = response.xpath('//div[contains(@class, "m-p-product")]')
+        for item in glasses:
+            yield{
+                'url': item.xpath(".//div[@class='pimg default-image-front']/a/@href").get(),
+                'img_url': item.xpath(".//div[@class='pimg default-image-front']/a/img[1]/@src").get(),
+                'name': item.xpath(".//div[@class='row']/p[contains(@class, 'pname')]/a/text()").get(),
+            }
